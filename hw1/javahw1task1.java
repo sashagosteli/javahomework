956224e61a25121cf73208805eb7c23cc36beed9
Alexandra <gosteli.alexandra@gmail.com>
2023-04-14 19:53:06 +0200

    task1 fixed

--- hw1/javahw1task1.java
+++ hw1/javahw1task1.java
@@ -1,7 +1,7 @@
 // Вычислить n-ое треугольного число (сумма чисел от 1 до n), n!
 // (произведение чисел от 1 до n)
 
-package javahw1;
+package javahw1.hw1;
 
 public class javahw1task1 {
     public static void main(String[] args) {
@@ -13,8 +13,8 @@ public class javahw1task1 {
         int sum = 1;
         for (int i = 1; i <= n; i++) {
             sum = i * sum;
-            System.out.println(sum);
-        }
 
+        }
+        System.out.println("Произведение чисел от 1 до " + n + " равно >>> " + sum);
     }
 }
