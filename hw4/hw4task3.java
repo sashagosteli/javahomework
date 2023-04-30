1230da1c33ee2ec07f08ad2e465b7b52046d81a1
Alexandra <gosteli.alexandra@gmail.com>
2023-04-30 22:16:51 +0200

    hw4 task 3 done

---
+++ hw4/hw4task3.java
@@ -0,0 +1,136 @@
+package javahw.hw4;
+
+import java.lang.System.Logger;
+import java.util.Scanner;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.SimpleFormatter;
+
+public class hw4task3 {
+
+    public static void main(String[] args) {
+
+        CalcPrint();
+
+    }
+
+    public static void CalcPrint() {
+        Scanner iScanner = new Scanner(System.in);
+        System.out.print("Введите 1 число : ");
+        int a = iScanner.nextInt();
+
+        System.out.print("Введите 2 число : ");
+        int b = iScanner.nextInt();
+
+        Scanner lineScanner = new Scanner(System.in);
+        System.out.print("Введите символ : ");
+        String c = lineScanner.nextLine();
+
+        int res = 0;
+        if (c.equals("*")) {
+            res = a * b;
+            System.out.println(a + " * " + b + " = " + res);
+        } else if (c.equals("+")) {
+            res = a + b;
+            System.out.println(a + " + " + b + " = " + res);
+        } else if (c.equals("-")) {
+            res = a - b;
+            System.out.println(a + " - " + b + " = " + res);
+        } else if (c.equals("/")) {
+            res = a / b;
+            System.out.println(a + " / " + b + " = " + res);
+        } else {
+            System.out.println("Наш калькулятор умеет только делить, умножать, вычитать и складывать(/, *, -, +)");
+        }
+        int newRes = 0;
+        System.out.print("Введите символ : ");
+        String nextc = lineScanner.nextLine();
+        System.out.print("Введите 2 число : ");
+        int d = iScanner.nextInt();
+
+        if (nextc.equals("*")) {
+            newRes = res * d;
+            System.out.println(res + " * " + d + " = " + newRes);
+        } else if (nextc.equals("+")) {
+            newRes = res + d;
+            System.out.println(res + " + " + d + " = " + newRes);
+        } else if (nextc.equals("-")) {
+            newRes = res - d;
+            System.out.println(res + " - " + d + " = " + newRes);
+        } else if (nextc.equals("/")) {
+            newRes = res / d;
+            System.out.println(res + " / " + d + " = " + newRes);
+        } else {
+            System.out.println("Наш калькулятор умеет только делить, умножать, вычитать и складывать(/, *, -, +)");
+        }
+
+        System.out.println("Введите символ или 'remove' чтобы отменить последнюю операцию: ");
+        nextc = lineScanner.nextLine();
+        if (nextc.equals("remove")) {
+            newRes = res;
+            System.out.println("результат = " + newRes);
+            System.out.print("Введите символ : ");
+            nextc = lineScanner.nextLine();
+        }
+        System.out.print("Введите 2 число : ");
+        d = iScanner.nextInt();
+
+        if (nextc.equals("*")) {
+            newRes = res * d;
+            System.out.println(res + " * " + d + " = " + newRes);
+        } else if (nextc.equals("+")) {
+            newRes = res + d;
+            System.out.println(res + " + " + d + " = " + newRes);
+        } else if (nextc.equals("-")) {
+            newRes = res - d;
+            System.out.println(res + " - " + d + " = " + newRes);
+        } else if (nextc.equals("/")) {
+            newRes = res / d;
+            System.out.println(res + " / " + d + " = " + newRes);
+        } else
+
+            iScanner.close();
+        lineScanner.close();
+
+    }
+
+    // public static void Calc() {
+    // Scanner iScanner = new Scanner(System.in);
+    // System.out.print("Введите 1 число : ");
+    // int a = iScanner.nextInt();
+
+    // System.out.print("Введите 2 число : ");
+    // int b = iScanner.nextInt();
+
+    // Scanner lineScanner = new Scanner(System.in);
+    // System.out.print("Введите символ : ");
+    // String c = lineScanner.nextLine();
+
+    // iScanner.close();
+    // lineScanner.close();
+    // CalcPrint(a, b, c);
+
+    // }
+
+    // public static void NextTask(int res) {
+    // Scanner nextScanner = new Scanner(System.in);
+    // System.out.print("Введите символ : ");
+    // String c = nextScanner.nextLine();
+    // System.out.print("Введите 2 число : ");
+    // int d = nextScanner.nextInt();
+    // int newRes = 0;
+    // if (c == "*") {
+    // newRes = res * d;
+    // System.out.println(res + "*" + d + "=" + newRes);
+    // } else if (c == "+") {
+    // newRes = res + d;
+    // System.out.println(res + "+" + d + "=" + newRes);
+    // } else if (c == "-") {
+    // newRes = res - d;
+    // System.out.println(res + "-" + d + "=" + newRes);
+    // } else if (c == "/") {
+    // newRes = res / d;
+    // System.out.println(res + "/" + d + "=" + newRes);
+    // }
+    // nextScanner.close();
+    // }
+}
